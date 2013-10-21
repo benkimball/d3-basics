8118da5ce76cda671f9817c358fbf2b097dcfcc8
Ben Kimball <github@benkimball.com>
2013-10-21 12:41:24 -0500

    Fix ordering bug, reorganize code, add category accessor.

--- README.md
+++ README.md
@@ -28,3 +28,13 @@ So far I've been ignoring the fact that this histogram is useless; it doesn't te
 Step 7: Axes
 
 Our first goal is to draw a horizontal axis with appropriate tick marks to show the values. Our second goal is to print the name of each category to the left of its associated bar. You could draw all of these things yourself by doing a bunch of math and then appending SVG text and line elements, but that would be dumb. Use D3!
+
+Step 8: Troubleshooting
+
+Wait a minute. What happened to our sorting? Our new Y axis shows us that we're no longer sorting by category name. How can we be sure that our plot is using the right data? Well, let's start by adding tooltips to the bars to make sure that we don't have a mismatch between the axis labels and the bars.
+
+Nope, okay, so what changed? Well, we're now using the output of the y() function to determine the vertical position of each bar, and y() is an axis object. Let's check its API: https://github.com/mbostock/d3/wiki/SVG-Axes#wiki-axis. Nope, nothing about sorting or ordering there. But remember that we created the axis using an ordinal scale object, so let's check that: https://github.com/mbostock/d3/wiki/Ordinal-Scales. Bingo! The description of the domain([values]) function says in part, "[i]f values is specified, sets the input domain of the ordinal scale to the specified array of values. The first element in values will be mapped to the first element in the output range, the second domain value to the second range value, and so on." And when we call axis.domain(), we're passing in an unsorted list of category names.
+
+So how do we fix it? First, for clarity, let's put all of our plotting code into a function and all of our data loading code into a different function. Now we just need to perform the data sort at the beginning of the plotting code.
+
+Finally, while we're cleaning things up, let's add an accessor functions for category, since we call that function repeatedly.
--- script.js
+++ script.js
@@ -1,79 +1,90 @@
 /* Generate arbitrary counts for each of eight Tumblr post types */
-var category_names = [
-  "video", "link", "photo", "audio", "chat", "quote", "answer", "text"
-]; 
-var data = rnd_hist(category_names);
-
-var canvas_width = 600,
-    canvas_height = 300;
-
-/* d3 "Conventional Margins": http://bl.ocks.org/mbostock/3019563 */
-var margin = {top:20, right:10, bottom:20, left:60};
-var width = canvas_width - margin.left - margin.right,
-    height = canvas_height - margin.top - margin.bottom;
-var svg = d3.select("body").append("svg")
-    .attr("class", "plot")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-/*
- * Set up scales for the X and Y axes.
- */
-var x = d3.scale.linear()
-        .domain([0, 100])
-        .rangeRound([0, width]),
-    y = d3.scale.ordinal()
-        .domain(category_names)
-        .rangeRoundBands([0, height], .1);
-
-/*
- * Set up SVG axes for the plot.
- */
-var xAxis = d3.svg.axis().scale(x),
-    yAxis = d3.svg.axis().scale(y).orient('left');
-
-/*
- * Draw the axes.
- */
-svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0,"+height+")")
-    .call(xAxis);
-svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis);
-
-/*
- * Bind data to plot element, and for each unrepresented datum, draw a
- * 10px tall left-aligned rectangle whose width in pixels matches its
- * category's count. Space each rectangle 20px down the page.
- */
-svg.selectAll(".category")
-    .data(data.sort(category_order))
-  .enter().append("rect")
-    .attr("class", "category")
-    .attr("x", 0)
-    .attr("width", function(d) { return x(d.count); })
-    .attr("y", function(d) { return y(d.category); })
-    .attr("height", y.rangeBand());
-
-
-/* ******************************************************************
- * Utility functions
- */
-function rnd_hist(labels) {
-  var hist = [], ct = 0;
-  labels.forEach(function(label) {
-    ct = Math.floor(Math.random()*100);
-    hist.push({category:label, count:ct});
-  });
-  return(hist);
+function load_data() {
+  var category_names = [
+    "video", "link", "photo", "audio", "chat", "quote", "answer", "text"
+  ]; 
+  function rnd_hist(labels) {
+    var hist = [], ct = 0;
+    labels.forEach(function(label) {
+      ct = Math.floor(Math.random()*100);
+      hist.push({category:label, count:ct});
+    });
+    return(hist);
+  }
+  return(rnd_hist(category_names))
 }
 
-function category_order(a, b) {
-  if(a.category > b.category) return 1;
-  else if(a.category < b.category) return -1;
-  return 0;
+/*
+ * Plot a histogram of category counts.
+ */
+function plot(data, dimensions) {
+  /* accessors */
+  function category(d) { return d.category; }
+  function count(d) { return d.count; }
+
+  /* comparators */
+  function category_order(a, b) {
+    if(a.category > b.category) return 1;
+    else if(a.category < b.category) return -1;
+    return 0;
+  }
+  
+  data = data.sort(category_order);
+
+  /* d3 "Conventional Margins": http://bl.ocks.org/mbostock/3019563 */
+  var margin = {top:20, right:10, bottom:20, left:60};
+  var width = dimensions.width - margin.left - margin.right,
+      height = dimensions.height - margin.top - margin.bottom;
+  var svg = d3.select("body").append("svg")
+      .attr("class", "plot")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  /*
+   * Set up scales for the X and Y axes.
+   */
+  var x = d3.scale.linear()
+          .domain([0, 100])
+          .rangeRound([0, width]),
+      y = d3.scale.ordinal()
+          .domain(data.map(category))
+          .rangeRoundBands([0, height], .1);
+
+  /*
+   * Set up SVG axes for the plot.
+   */
+  var xAxis = d3.svg.axis().scale(x),
+      yAxis = d3.svg.axis().scale(y).orient('left');
+
+  /*
+   * Draw the axes.
+   */
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0,"+height+")")
+      .call(xAxis);
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
+
+  /*
+   * Bind data to plot element, and for each unrepresented datum, draw a
+   * 10px tall left-aligned rectangle whose width in pixels matches its
+   * category's count. Space each rectangle 20px down the page.
+   */
+  svg.selectAll(".category")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "category")
+      .attr("x", 0)
+      .attr("width", function(d) { return x(d.count); })
+      .attr("y", function(d) { return y(d.category); })
+      .attr("height", y.rangeBand())
+    .append("title")
+      .text(category)
+  
 }
+
+plot(load_data(), {width: 600, height: 300})
