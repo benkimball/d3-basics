07bc9a26d088d3cdc8debfec9892b2f0e39ae94b
Ben Kimball <github@benkimball.com>
2013-10-21 11:33:41 -0500

    Local data, plot SVG rectangles in the most basic way.

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
+/* Generate arbitrary counts for each of eight Tumblr post types */
 var data = rnd_hist([
   "video", "link", "photo", "audio", "chat", "quote", "answer", "text"
 ]);
 
-d3.select("body").selectAll(".category")
-    .data(data.sort(category_order))
-  .enter().append("p")
-    .attr("class", "category")
-    .html(function(d) {
-      return "The '"+d.category+"' category contains "+d.count+" thing(s).";
-    });
+/* Create SVG element */
+var svg = d3.select("body").append("svg")
+    .attr("class", "plot")
+    .attr("width", 600)
+    .attr("height", 300);
 
+/*
+ * Bind data to plot element, and for each unrepresented datum, draw a
+ * 10px tall left-aligned rectangle whose width in pixels matches its
+ * category's count. Space each rectangle 20px down the page.
+ */
+svg.selectAll(".category")
+    .data(data.sort(category_order))
+  .enter().append("rect")
+    .attr("class", "category")
+    .attr("x", 0)
+    .attr("width", function(d) { return d.count; })
+    .attr("y", function(d, i) { return i * 20; })
+    .attr("height", 10)
+
+
+/* ******************************************************************
+ * Utility functions
+ */
 function rnd_hist(labels) {
   var hist = [], ct = 0;
   labels.forEach(function(label) {
